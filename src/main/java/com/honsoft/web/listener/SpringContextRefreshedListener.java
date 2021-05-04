a0eec29acd85cc635a998add0e1b287cc1fa7813
changnam go <changnam.go@gmail.com>
2021-05-04 12:35:59 +0900

    springweb initial commit

---
+++ src/main/java/com/honsoft/web/listener/SpringContextRefreshedListener.java
@@ -0,0 +1,21 @@
+package com.honsoft.web.listener;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		System.out.println("Handling context re-freshed event. ");
+		
+		System.out.println(" beans count in applicationContext: "+event.getApplicationContext().getBeanDefinitionCount());
+		
+		String[] beanNames = event.getApplicationContext().getBeanDefinitionNames();
+		for (String beanName : beanNames)
+			System.out.println(beanName);
+		
+		if (event.getApplicationContext().getParent() != null)
+			System.out.println(" beans count in parent applicationContext: "+event.getApplicationContext().getParent().getBeanDefinitionCount());
+	}
+}
